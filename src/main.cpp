5c44cce0d90f31ef7325c34bb0268a1c5868ea83
wararyo <wararyo@gmail.com>
2022-02-23 04:23:13 +0900

    正弦波からピアノの単音に置き換え

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <M5Core2.h>
 #include <driver/i2s.h>
 
-extern const unsigned char previewR[396900];
+extern const int16_t piano_sample[128000];
 
 #define CONFIG_I2S_BCK_PIN 12
 #define CONFIG_I2S_LRCK_PIN 0
@@ -19,6 +19,87 @@ extern const unsigned char previewR[396900];
 constexpr uint64_t TIMER_INTERVAL = (uint64_t)(SAMPLE_BUFFER_SIZE * 1000000 / SAMPLE_RATE);
 
 hw_timer_t *audioProcessTimer = NULL;
+
+enum SampleAdsr
+{
+  attack,
+  decay,
+  sustain,
+  release,
+};
+
+struct Sample
+{
+  const int16_t *sample;
+  uint32_t length;
+  uint8_t root;
+  uint32_t loop_start;
+  uint32_t loop_end;
+
+  uint32_t attack;
+  uint32_t decay;
+  float sustain;
+  uint32_t release;
+};
+
+struct SamplePlayer
+{
+  SamplePlayer(struct Sample *sample, float pitch, float volume) : sample{sample}, pitch{pitch}, volume{volume} {}
+  struct Sample *sample;
+  float pitch;
+  float volume;
+  uint32_t pos = 0;
+  float pos_f = 0.0f;
+  bool playing = true;
+  float adsr_gain = 0.0f;
+  enum SampleAdsr adsr_state = SampleAdsr::attack;
+};
+
+struct Sample piano = Sample{
+    piano_sample,
+    128000,
+    60,
+    22971,
+    23649,
+    0,
+    132300,
+    0.1f,
+    13230};
+
+struct SamplePlayer samplePlayer = SamplePlayer{
+    &piano,
+    1.0f,
+    1.0f};
+
+void IRAM_ATTR AudioLoop()
+{
+  int16_t sampleDataU[SAMPLE_BUFFER_SIZE];
+
+  if (samplePlayer.playing == false)
+    return;
+
+  // 波形を生成
+  for (int n = 0; n < SAMPLE_BUFFER_SIZE; n++)
+  {
+    if (samplePlayer.pos >= samplePlayer.sample->length)
+    {
+      samplePlayer.playing = false;
+      sampleDataU[n] = 0;
+    }
+    else
+    {
+      // 正弦波を生成
+      sampleDataU[n] = samplePlayer.sample->sample[samplePlayer.pos];
+      // sampleDataU[n] *= samplePlayer.volume;
+    }
+
+    samplePlayer.pos += 1;
+  }
+
+  static size_t bytes_written = 0;
+  i2s_write(Speak_I2S_NUMBER, (const unsigned char *)sampleDataU, 2 * SAMPLE_BUFFER_SIZE, &bytes_written, portMAX_DELAY);
+  printf("%d\n", bytes_written);
+}
 
 bool InitI2SSpeakOrMic(int mode)
 {
@@ -58,44 +139,18 @@ bool InitI2SSpeakOrMic(int mode)
   return true;
 }
 
-void DisplayInit(void)
-{
-  M5.Lcd.fillScreen(WHITE);
-  M5.Lcd.setTextColor(BLACK);
-  M5.Lcd.setTextSize(2);
-}
-
 void SpeakInit(void)
 {
   M5.Axp.SetSpkEnable(true);
   InitI2SSpeakOrMic(MODE_SPK);
 }
 
-void DingDong(void)
-{
-  size_t bytes_written = 0;
-  i2s_write(Speak_I2S_NUMBER, previewR, 396900, &bytes_written, portMAX_DELAY);
-}
-
-void IRAM_ATTR AudioLoop()
-{
-  int16_t sampleDataU[SAMPLE_BUFFER_SIZE];
-
-  for (int n = 0; n < SAMPLE_BUFFER_SIZE; n++)
-  {
-    // 正弦波を生成
-    sampleDataU[n] = int16_t(sin(n * 2.0f * PI / 64.0f) * 32767.0f); //64サンプルで1周期
-  }
-
-  static size_t bytes_written = 0;
-  i2s_write(Speak_I2S_NUMBER, (const unsigned char *)sampleDataU, 2 * SAMPLE_BUFFER_SIZE, &bytes_written, portMAX_DELAY);
-  printf("%d\n", bytes_written);
-}
-
 void setup()
 {
   M5.begin(true, true, true, true);
-  DisplayInit();
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.setTextColor(BLACK);
+  M5.Lcd.setTextSize(2);
   M5.Lcd.setTextColor(RED);
   M5.Lcd.setCursor(10, 10);
   M5.Lcd.printf("Sampler Test");
@@ -103,7 +158,8 @@ void setup()
   M5.Lcd.setCursor(10, 26);
   M5.Lcd.printf("Touch to listen sin wave");
   SpeakInit();
-  DingDong();
+  size_t bytes_written = 0;
+  i2s_write(Speak_I2S_NUMBER, (const unsigned char *)piano_sample, 256000, &bytes_written, portMAX_DELAY);
   delay(100);
 
   // audioProcessTimer = timerBegin(0, 80, true);
@@ -115,8 +171,10 @@ void setup()
 void loop()
 {
   TouchPoint_t pos = M5.Touch.getPressPoint();
-  if (pos.y > 0) {
+  if (pos.y > 0)
+  {
     AudioLoop();
   }
-  else delay(10);
+  else
+    delay(10);
 }
